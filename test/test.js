474005076fc414cbb88f8920927a3a4dea494e4c
Roben Kleene <robenk@gmail.com>
2013-12-19 21:39:11 -0500

    activeElement (focus) tests

--- test/test.js
+++ test/test.js
@@ -4,11 +4,13 @@ var constants = require('./lib/constants');
 describe('Bullets', function() {
 	var dommanager;
 	var testhelper;
+	var w;
 
 	before(function(done) {
 		dommanager = require(constants.dommanagerFile);
 		testhelper = require(constants.testhelperFile);
 		dommanager.loadFile(constants.HTMLFile, function(window) { 
+			w = window;
 			$ = window.$
 			Bullets = window.Bullets;
 			testhelper.init(window);
@@ -25,12 +27,14 @@ describe('Bullets', function() {
 			Bullets.selection.length.should.equal(1);
 			Bullets.deselect(Bullets.selection);
 			Bullets.selection.length.should.equal(0);
+			w.document.activeElement.should.equal(w.document.body);
 		});
 		it('should deselect the selection when nothing is passed in', function () {
 			Bullets.selectNext();
 			Bullets.selection.length.should.equal(1);
 			Bullets.deselect();
 			Bullets.selection.length.should.equal(0);
+			w.document.activeElement.should.equal(w.document.body);
 		});
 	});
 
@@ -40,25 +44,40 @@ describe('Bullets', function() {
 	    });
 		it('should select the first tag when nothing is selected', function() {
 			Bullets.selectNext();
+
+			Bullets.selection.length.should.equal(1);
+			var testSelection = w.document.activeElement;
+			var selection = Bullets.selection[0];
+			selection.should.equal(testSelection);
+
 			var testHelperText = testhelper.textOf('a', 0);
 			var bulletsText = Bullets.selection.text();
-			Bullets.selection.length.should.equal(1);
 			bulletsText.should.equal(testHelperText);
 		});
 		it('should select the next tag after the selection', function() {
 			Bullets.selectNext();
 			Bullets.selectNext();
+
+			Bullets.selection.length.should.equal(1);
+			var testSelection = w.document.activeElement;
+			var selection = Bullets.selection[0];
+			selection.should.equal(testSelection);
+			
 			var bulletsText = Bullets.selection.text();
 			var testHelperText = testhelper.textOf('a', 1);
-			Bullets.selection.length.should.equal(1);
 			bulletsText.should.equal(testHelperText);
 		});
 		it('should keep the same selected tag when the last tag is selected', function() {
 			Bullets.selectPrevious();
 			Bullets.selectNext();
-			var bulletsText = Bullets.selection.text();
-			var testHelperText = testhelper.textOf('a', 1);
+
 			Bullets.selection.length.should.equal(1);
+			var testSelection = w.document.activeElement;
+			var selection = Bullets.selection[0];
+			selection.should.equal(testSelection);
+
+			var bulletsText = Bullets.selection.text();
+			var testHelperText = testhelper.textOf('a', -1);
 			bulletsText.should.equal(testHelperText);
 		});
 	});
@@ -69,37 +88,42 @@ describe('Bullets', function() {
 	    });
 		it('should select the last tag when nothing is selected', function() {
 			Bullets.selectPrevious();
-			var testHelperText = testhelper.textOf('a', 1);
-			var bulletsText = Bullets.selection.text();
+
 			Bullets.selection.length.should.equal(1);
+			var testSelection = w.document.activeElement;
+			var selection = Bullets.selection[0];
+			selection.should.equal(testSelection);
+
+			var testHelperText = testhelper.textOf('a', -1);
+			var bulletsText = Bullets.selection.text();
 			bulletsText.should.equal(testHelperText);
 		});
 		it('should select the previous tag before the selection', function() {
 			Bullets.selectPrevious();
 			Bullets.selectPrevious();
-			var bulletsText = Bullets.selection.text();
-			var testHelperText = testhelper.textOf('a', 0);
+
 			Bullets.selection.length.should.equal(1);
+			var testSelection = w.document.activeElement;
+			var selection = Bullets.selection[0];
+			selection.should.equal(testSelection);
+
+			var bulletsText = Bullets.selection.text();
+			var testHelperText = testhelper.textOf('a', -2);
 			bulletsText.should.equal(testHelperText);
 		});
 		it('should keep the same selected tag when the first tag is selected', function() {
 			Bullets.selectNext();
 			Bullets.selectPrevious();
+
+			Bullets.selection.length.should.equal(1);
+			var testSelection = w.document.activeElement;
+			var selection = Bullets.selection[0];
+			selection.should.equal(testSelection);
+
 			var bulletsText = Bullets.selection.text();
 			var testHelperText = testhelper.textOf('a', 0);
-			Bullets.selection.length.should.equal(1);
 			bulletsText.should.equal(testHelperText);
 		});
 	});
-
-	// TODO Write tests based on these handlers
-	// $(document).ready(function () {
-	// 	$(Bullets.tags).focus(function() {
-	// 		$(this).attr('id', Bullets.selected);
-	// 	});
-	// 	$(Bullets.tags).blur(function() {
-	// 		$(this).removeAttr('id');
-	// 	});
-	// });
 
 });
