930d3cd46094d8f1c9f590b6c41a40ac562d38ba
Roben Kleene <robenk@gmail.com>
2015-05-04 21:21:08 -0400

    Setup gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,33 @@
 var gulp = require('gulp');
-
 var jade = require('gulp-jade');
- 
-gulp.task('jade-test', function() { 
+
+
+// Jade
+
+gulp.task('jade-example', function() {
+  gulp.src('./example/src/jade/index.jade')
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest('./example/build/'));
+});
+
+gulp.task('jade-test', function() {
   gulp.src('./test/src/jade/index.jade')
     .pipe(jade({
       pretty: true
     }))
-    .pipe(gulp.dest('./test/build/'))
+    .pipe(gulp.dest('./test/build/'));
 });
 
-// gulp.task('jade', ['jade-tests', 'jade-example']);
-gulp.task('jade', ['jade-test']);
+
+// Watch
+
+gulp.task('watch', function() {
+    gulp.watch('./test/src/jade/*.*', ['jade-test']);
+    // On example change also build test because test imports example
+    gulp.watch('./example/src/jade/*.*', ['jade-example', 'jade-test']);
+});
+
+gulp.task('jade', ['jade-test', 'jade-example']);
 gulp.task('default', ['jade']);
