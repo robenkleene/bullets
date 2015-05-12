127c0b00ea78173fdad1a8f6cb274eaa027dc78d
Roben Kleene <robenk@gmail.com>
2015-05-11 21:54:58 -0400

    Remove focus tests

--- test/build/js/tests.js
+++ test/build/js/tests.js
@@ -1,74 +1,4 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-describe('Bullets focus and blur', function() {
-  beforeEach(function() {
-		Bullets.deselect();
-	});
-
-  it('only focus should run when the next tag is selected', function() {
-    var element = Bullets.elementAtOffset(1);
-    var focusSpy = sinon.spy(element, 'focus');
-    var blurSpy = sinon.spy(element, 'blur');
-    Bullets.selectNext();
-    focusSpy.should.have.callCount(1);
-    blurSpy.should.have.callCount(0);
-    focusSpy.restore();
-    blurSpy.restore();
-  });
-
-  it('focus and blur should not run when the selection doesn\'t change', function() {
-    var element = Bullets.elementAtOffset(1);
-    var focusSpy = sinon.spy(element, 'focus');
-    var blurSpy = sinon.spy(element, 'blur');
-    var stub = sinon.stub(Bullets, 'nothingToSelect');
-
-    Bullets.selectNext();
-    Bullets.selectPrevious();
-    focusSpy.should.have.callCount(1);
-    blurSpy.should.have.callCount(0);
-    stub.should.have.callCount(1);
-
-    stub.restore();
-    focusSpy.restore();
-    blurSpy.restore();
-  });
-
-  it('focus and blur should each fire once when the selection changes', function() {
-    var elementOne = Bullets.elementAtOffset(1);
-    var focusSpyOne = sinon.spy(elementOne, 'focus');
-    var blurSpyOne = sinon.spy(elementOne, 'blur');
-    Bullets.selectNext();
-
-    var elementTwo = Bullets.elementAtOffset(1);
-    var focusSpyTwo = sinon.spy(elementTwo, 'focus');
-    var blurSpyTwo = sinon.spy(elementTwo, 'blur');
-    Bullets.selectNext();
-    focusSpyOne.should.have.callCount(1);
-    blurSpyOne.should.have.callCount(1);
-    focusSpyTwo.should.have.callCount(1);
-    blurSpyTwo.should.have.callCount(0);
-
-
-    var elementThree = Bullets.elementAtOffset(1);
-    var focusSpyThree = sinon.spy(elementThree, 'focus');
-    var blurSpyThree = sinon.spy(elementThree, 'blur');
-    Bullets.selectNext();
-    focusSpyOne.should.have.callCount(1);
-    blurSpyOne.should.have.callCount(1);
-    focusSpyTwo.should.have.callCount(1);
-    blurSpyTwo.should.have.callCount(1);
-    focusSpyThree.should.have.callCount(1);
-    blurSpyThree.should.have.callCount(0);
-
-    focusSpyOne.restore();
-    blurSpyOne.restore();
-    focusSpyTwo.restore();
-    blurSpyTwo.restore();
-    focusSpyThree.restore();
-    blurSpyThree.restore();
-  });
-});
-
-},{}],2:[function(require,module,exports){
 var testHelper = require('../js/test-helper');
 window.testHelper = testHelper;
 
@@ -163,7 +93,7 @@ describe('Bullets selection', function() {
 	});
 });
 
-},{"../js/test-helper":3}],3:[function(require,module,exports){
+},{"../js/test-helper":2}],2:[function(require,module,exports){
 module.exports = {
 
 	testNoSelectedElement: function() {
@@ -220,4 +150,4 @@ module.exports = {
 
 };
 
-},{}]},{},[1,2]);
+},{}]},{},[1]);
