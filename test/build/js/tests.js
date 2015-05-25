125f76bfa4a02686c93565ea4dbbe9c32b80c0da
Roben Kleene <robenk@gmail.com>
2015-05-25 16:25:56 -0400

    Change tabs to spaces

--- test/build/js/tests.js
+++ test/build/js/tests.js
@@ -43,121 +43,126 @@ describe('Bullets visible elements', function() {
   });
   describe('Collapse Selection Tests', function() {
     beforeEach(function() {
-  		Bullets.rootElement = document.getElementById("collapse-selection-tests");
-  	});
+      Bullets.rootElement = document.getElementById("collapse-selection-tests");
+    });
     describe('Collapse Selection Test 1', function() {
-  		it('should find the correct element', function () {
+      it('should find the correct element', function () {
         runBothTests('cs-test-1');
-  		});
-  	});
+      });
+    });
     describe('Collapse Selection Test 2', function() {
-  		it('should find the correct element', function () {
+      it('should find the correct element', function () {
         runBothTests('cs-test-2');
-  		});
-  	});
+      });
+    });
     describe('Collapse Selection Test 3', function() {
-  		it('should find the correct element', function () {
+      it('should find the correct element', function () {
         runBothTests('cs-test-3');
-  		});
-  	});
+      });
+    });
     describe('Collapse Selection Test 4', function() {
-  		it('should find the correct element', function () {
+      it('should find the correct element', function () {
         runBothTests('cs-test-4');
-  		});
-  	});
+      });
+    });
     describe('Collapse Selection Test 5', function() {
-  		it('should find the correct element', function () {
+      it('should find the correct element', function () {
         runBothTests('cs-test-5');
-  		});
-  	});
+      });
+    });
     describe('Collapse Selection Test 6', function() {
-  		it('should find the correct element', function () {
+      it('should find the correct element', function () {
         runBothTests('cs-test-6');
-  		});
-  	});
+      });
+    });
     describe('Collapse Selection Test 7', function() {
-  		it('should find the correct element', function () {
+      it('should find the correct element', function () {
         runBothTests('cs-test-7');
-  		});
-  	});
+      });
+    });
     describe('Collapse Selection Test 8', function() {
-  		it('should find the correct element', function () {
+      it('should find the correct element', function () {
         runBothTests('cs-test-8');
-  		});
-  	});
+      });
+    });
     describe('Collapse Selection Test 9', function() {
-  		it('should find the correct element', function () {
+      it('should find the correct element', function () {
         runBothTests('cs-test-9');
-  		});
-  	});
+      });
+    });
     describe('Collapse Selection Test 10', function() {
-  		it('should find the correct element', function () {
+      it('should find the correct element', function () {
         runBothTests('cs-test-10');
-  		});
-  	});
+      });
+    });
     describe('Collapse Selection Test 11', function() {
-  		it('should find the correct element', function () {
+      it('should find the correct element', function () {
         runBothTests('cs-test-11');
-  		});
-  	});
+      });
+    });
     describe('Collapse Selection Test 12', function() {
-  		it('should find the correct element', function () {
+      it('should find the correct element', function () {
         runBothTests('cs-test-12');
-  		});
-  	});
+      });
+    });
     describe('Collapse Selection Test 13', function() {
-  		it('should find the correct element', function () {
+      it('should find the correct element', function () {
         runBothTests('cs-test-13');
-  		});
-  	});
+      });
+    });
     describe('Collapse Selection Test 14', function() {
-  		it('should find the correct element', function () {
+      it('should find the correct element', function () {
         runBothTests('cs-test-14');
-  		});
-  	});
+      });
+    });
+    describe('Collapse Selection Test 15', function() {
+      it('should find the correct element', function () {
+        runBothTests('cs-test-15');
+      });
+    });
   });
 
   describe('Header Collapse Selection Tests', function() {
     beforeEach(function() {
-  		Bullets.rootElement = document.getElementById("header-collapse-selection-tests");
-  	});
+      Bullets.rootElement = document.getElementById("header-collapse-selection-tests");
+    });
     describe('Header Collapse Selection Test 1', function() {
-  		it('should find the correct element', function () {
+      it('should find the correct element', function () {
         runBothTests('hcs-test-1');
-  		});
-  	});
+      });
+    });
     describe('Header Collapse Selection Test 2', function() {
-  		it('should find the correct element', function () {
+      it('should find the correct element', function () {
         runBothTests('hcs-test-2');
-  		});
-  	});
+      });
+    });
     describe('Header Collapse Selection Test 3', function() {
-  		it('should find the correct element', function () {
+      it('should find the correct element', function () {
         runBothTests('hcs-test-3');
-  		});
-  	});
+      });
+    });
     describe('Header Collapse Selection Test 4', function() {
-  		it('should find the correct element', function () {
+      it('should find the correct element', function () {
         runBothTests('hcs-test-4');
-  		});
-  	});
+      });
+    });
     describe('Header Collapse Selection Test 5', function() {
-  		it('should find the correct element', function () {
+      it('should find the correct element', function () {
         runBothTests('hcs-test-5');
-  		});
-  	});
+      });
+    });
     describe('Header Collapse Selection Test 6', function() {
-  		it('should find the correct element', function () {
+      it('should find the correct element', function () {
         runBothTests('hcs-test-6');
-  		});
-  	});
+      });
+    });
     describe('Header Collapse Selection Test 7', function() {
-  		it('should find the correct element', function () {
+      it('should find the correct element', function () {
         runBothTests('hcs-test-7');
-  		});
-  	});
+      });
+    });
 
-	});
+  });
 
 });
 
