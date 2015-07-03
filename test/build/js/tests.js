683d0e12b6c2582c685f3b4db82dba99776662ac
Roben Kleene <robenk@gmail.com>
2015-07-03 16:25:01 -0400

    Make dist folder

--- test/build/js/tests.js
+++ test/build/js/tests.js
@@ -1,4 +1,94 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+module.exports = {
+
+	testNoSelectedElement: function() {
+		var nodeList = Bullets.selectedNodes;
+		nodeList.length.should.equal(0);
+		Bullets.rootElement.querySelectorAll(Bullets.selectedClass).length.should.equal(0);
+	},
+	testSelectionMatchesIndex: function(index) {
+    // Confirm a selected element exists
+		var selectedElement = Bullets.selectedNodes[0];
+		selectedElement.classList.contains(Bullets.selectedClass).should.equal(true);
+
+    // Confirm that the inner text of that element equals
+    // the inner text of the element at the index
+		var testText = this.textOfBulletsElementAtIndex(index);
+		var bulletsText = selectedElement.innerText;
+		bulletsText.should.equal(testText);
+	},
+
+	isSelectedElementCollapsed: function() {
+		var selectedElement = Bullets.selectedNodes[0];
+		if (!selectedElement) {
+			return false;
+		}
+		return selectedElement.classList.contains(Bullets.collapsedClass);
+	},
+
+	indexOfBulletsElementWithChildMatchingQuerySelector: function(selectors) {
+		return this.indexOfBulletsElementUsingChildMatchingQuerySelector(selectors, false);
+	},
+
+	indexOfBulletsElementWithoutChildMatchingQuerySelector: function(selectors) {
+		return this.indexOfBulletsElementUsingChildMatchingQuerySelector(selectors, true);
+	},
+
+	indexOfBulletsElementUsingChildMatchingQuerySelector: function(selectors, invert) {
+		var selectableNodeList = Bullets.rootElement.querySelectorAll(Bullets.selectTags);
+		for (var i = 0; i < selectableNodeList.length; i++) {
+	    var selectableElement = selectableNodeList[i];
+			var followNodeList = selectableElement.querySelectorAll(selectors);
+			if (!invert) {
+				if (followNodeList.length > 0) {
+					return i;
+				}
+			} else {
+				if (followNodeList.length < 1) {
+					return i;
+				}
+			}
+		}
+	},
+
+	// Bullets Elements
+
+	textOfBulletsElementAtIndex: function(index) {
+		return this.textOfQuerySelectorAtIndex(Bullets.selectTags, index);
+	},
+
+	valueOfAttributeForBulletsElementAtIndex: function(selectors, index, attribute) {
+		return this.valueOfAttributeForQuerySelectorAtIndex(Bullets.selectTags, index, attribute);
+	},
+
+	bulletsElementAtIndex: function(index) {
+		return this.elementOfQuerySelectorAtIndex(Bullets.selectTags, index);
+	},
+
+
+	// Element Helpers
+
+	textOfQuerySelectorAtIndex: function(selectors, index) {
+		var node = this.elementOfQuerySelectorAtIndex(selectors, index);
+		return node.innerText;
+	},
+
+	valueOfAttributeForQuerySelectorAtIndex: function(selectors, index, attribute) {
+		var node = this.elementOfQuerySelectorAtIndex(selectors, index);
+		return node.getAttribute(attribute);
+	},
+
+	elementOfQuerySelectorAtIndex: function (selectors, index) {
+		var nodeList = Bullets.rootElement.querySelectorAll(selectors);
+		if (index < 0) {
+			index = nodeList.length + index;
+		}
+		return nodeList[index];
+	}
+
+};
+
+},{}],2:[function(require,module,exports){
 if (typeof Element.prototype.matches === 'undefined') {
   // `Element.prototype.matches` appears to be missing from phantomjs
   Element.prototype.matches = Element.prototype.webkitMatchesSelector;
@@ -166,7 +256,7 @@ describe('Bullets visible elements', function() {
 
 });
 
-},{}],2:[function(require,module,exports){
+},{}],3:[function(require,module,exports){
 var testHelper = require('../js/test-helper');
 window.testHelper = testHelper;
 
@@ -248,7 +338,7 @@ describe('Bullets collapse', function() {
   });
 });
 
-},{"../js/test-helper":4}],3:[function(require,module,exports){
+},{"../js/test-helper":1}],4:[function(require,module,exports){
 var testHelper = require('../js/test-helper');
 window.testHelper = testHelper;
 
@@ -376,94 +466,4 @@ describe('Bullets selection', function() {
 	});
 });
 
-},{"../js/test-helper":4}],4:[function(require,module,exports){
-module.exports = {
-
-	testNoSelectedElement: function() {
-		var nodeList = Bullets.selectedNodes;
-		nodeList.length.should.equal(0);
-		Bullets.rootElement.querySelectorAll(Bullets.selectedClass).length.should.equal(0);
-	},
-	testSelectionMatchesIndex: function(index) {
-    // Confirm a selected element exists
-		var selectedElement = Bullets.selectedNodes[0];
-		selectedElement.classList.contains(Bullets.selectedClass).should.equal(true);
-
-    // Confirm that the inner text of that element equals
-    // the inner text of the element at the index
-		var testText = this.textOfBulletsElementAtIndex(index);
-		var bulletsText = selectedElement.innerText;
-		bulletsText.should.equal(testText);
-	},
-
-	isSelectedElementCollapsed: function() {
-		var selectedElement = Bullets.selectedNodes[0];
-		if (!selectedElement) {
-			return false;
-		}
-		return selectedElement.classList.contains(Bullets.collapsedClass);
-	},
-
-	indexOfBulletsElementWithChildMatchingQuerySelector: function(selectors) {
-		return this.indexOfBulletsElementUsingChildMatchingQuerySelector(selectors, false);
-	},
-
-	indexOfBulletsElementWithoutChildMatchingQuerySelector: function(selectors) {
-		return this.indexOfBulletsElementUsingChildMatchingQuerySelector(selectors, true);
-	},
-
-	indexOfBulletsElementUsingChildMatchingQuerySelector: function(selectors, invert) {
-		var selectableNodeList = Bullets.rootElement.querySelectorAll(Bullets.selectTags);
-		for (var i = 0; i < selectableNodeList.length; i++) {
-	    var selectableElement = selectableNodeList[i];
-			var followNodeList = selectableElement.querySelectorAll(selectors);
-			if (!invert) {
-				if (followNodeList.length > 0) {
-					return i;
-				}
-			} else {
-				if (followNodeList.length < 1) {
-					return i;
-				}
-			}
-		}
-	},
-
-	// Bullets Elements
-
-	textOfBulletsElementAtIndex: function(index) {
-		return this.textOfQuerySelectorAtIndex(Bullets.selectTags, index);
-	},
-
-	valueOfAttributeForBulletsElementAtIndex: function(selectors, index, attribute) {
-		return this.valueOfAttributeForQuerySelectorAtIndex(Bullets.selectTags, index, attribute);
-	},
-
-	bulletsElementAtIndex: function(index) {
-		return this.elementOfQuerySelectorAtIndex(Bullets.selectTags, index);
-	},
-
-
-	// Element Helpers
-
-	textOfQuerySelectorAtIndex: function(selectors, index) {
-		var node = this.elementOfQuerySelectorAtIndex(selectors, index);
-		return node.innerText;
-	},
-
-	valueOfAttributeForQuerySelectorAtIndex: function(selectors, index, attribute) {
-		var node = this.elementOfQuerySelectorAtIndex(selectors, index);
-		return node.getAttribute(attribute);
-	},
-
-	elementOfQuerySelectorAtIndex: function (selectors, index) {
-		var nodeList = Bullets.rootElement.querySelectorAll(selectors);
-		if (index < 0) {
-			index = nodeList.length + index;
-		}
-		return nodeList[index];
-	}
-
-};
-
-},{}]},{},[1,2,3]);
+},{"../js/test-helper":1}]},{},[2,3,4]);
